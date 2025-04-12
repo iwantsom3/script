9f85add0b15f9d57329c32ab2df58cfccb0ec6e6
iwantsom3 <85751769+iwantsom3@users.noreply.github.com>
2025-04-12 09:03:34 +0900

    Create UnliHatch.lua

---
+++ UnliHatch.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iwantsom3/script/refs/heads/main/Hatch.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/No"))()
