df5b8afc1d3aad0896a39a5d73d5f8edc56e4d40
iwantsom3 <85751769+iwantsom3@users.noreply.github.com>
2025-05-11 01:01:50 +0900

    Update Spawns.lua

--- Spawns.lua
+++ Spawns.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/No"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/Spawn.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PlsNoNoob/-/refs/heads/main/No"))()
